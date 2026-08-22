2e5854ae9e8911c3ca10e93039dc187a650e1ea5
Haris Rindh <haris.rindh.pk@gmail.com>
2026-08-22 22:37:08 +0500

    Setup GitHub Pages and auto-deploy workflow

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,11 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
   plugins: [react()],
+  base: mode === 'production' ? '/DigiHustler/' : '/',
   server: {
     port: 3000,
     open: false
   }
-});
+}));
